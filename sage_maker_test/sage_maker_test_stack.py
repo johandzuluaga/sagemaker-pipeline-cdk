c58e3dbb19a671291dd7146de9bfb8613c8b55b1
johandzuluaga <johand.zuluaga@udea.edu.co>
2025-07-05 20:33:31 -0500

    feat: created buckets for raw and processed data and titanic dataset upload

--- sage_maker_test/sage_maker_test_stack.py
+++ sage_maker_test/sage_maker_test_stack.py
@@ -1,19 +1,36 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_s3 as s3,
+    aws_s3_deployment as s3deploy,
+    RemovalPolicy,
 )
 from constructs import Construct
+from pathlib import Path
 
 class SageMakerTestStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        # Raw data bucket
+        raw_data_bucket = s3.Bucket(self, "RawDataBucket",
+            bucket_name="ml-raw-data-johan",
+            versioned=True,
+            removal_policy=RemovalPolicy.DESTROY,
+            auto_delete_objects=True
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "SageMakerTestQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # Processed data bucket
+        processed_data_bucket = s3.Bucket(self, "ProcessedDataBucket",
+            bucket_name="ml-processed-data-johan",
+            versioned=True,
+            removal_policy=RemovalPolicy.DESTROY,
+            auto_delete_objects=True
+        )
+
+        # Deploy Titanic CSV to raw bucket
+        s3deploy.BucketDeployment(self, "UploadTitanicDataset",
+            destination_bucket=raw_data_bucket,
+            sources=[s3deploy.Source.asset(str(Path(__file__).parent.parent / "data"))],
+            destination_key_prefix="titanic",
+        )
