8da754e5f477e942ca98feb3d5a40db9665b8b90
johandzuluaga <johand.zuluaga@udea.edu.co>
2025-07-06 23:57:25 -0500

    Add model registration to the pipeline

--- pipeline/pipeline_definition.py
+++ pipeline/pipeline_definition.py
@@ -1,15 +1,22 @@
-from pathlib import Path
 import sagemaker
+# Data preprocessing imports
 from sagemaker.sklearn.processing import SKLearnProcessor
 from sagemaker.workflow.steps import ProcessingStep
 from sagemaker.workflow.pipeline import Pipeline
 from sagemaker.processing import ProcessingInput, ProcessingOutput
+# Model training imports
 from sagemaker.sklearn.estimator import SKLearn
 from sagemaker.workflow.steps import TrainingStep
 from sagemaker.inputs import TrainingInput
-import os
+# Model registration imports
+from sagemaker.workflow.model_step import ModelStep
+from sagemaker.model_metrics import MetricsSource, ModelMetrics
+from sagemaker.workflow.model_step import ModelStep
+from sagemaker.sklearn.model import SKLearnModel
+from sagemaker.workflow.pipeline_context import PipelineSession
 
-sagemaker_session = sagemaker.session.Session()
+pipeline_session = PipelineSession()
+# sagemaker_session = sagemaker.session.Session()
 role_arn = "arn:aws:iam::728611193981:role/SageMakerTestStack-SageMakerExecutionRole7843F3B8-ue6AtEtiCRPx"
 bucket_raw = "ml-raw-data-johan"
 bucket_processed = "ml-processed-data-johan"
@@ -20,7 +27,7 @@ script_processor = SKLearnProcessor(
     instance_type="ml.t3.medium",
     instance_count=1,
     base_job_name="titanic-preprocessing",
-    sagemaker_session=sagemaker_session
+    sagemaker_session=pipeline_session
 )
 
 processing_step = ProcessingStep(
@@ -50,7 +57,7 @@ sklearn_estimator = SKLearn(
     framework_version="0.23-1",
     instance_count=1,
     base_job_name="titanic-train",
-    sagemaker_session=sagemaker_session,
+    sagemaker_session=pipeline_session,
 )
 
 # Define TrainingStep
@@ -65,10 +72,44 @@ training_step = TrainingStep(
     },
 )
 
+# # Create model metrics object
+# model_metrics = ModelMetrics(
+#     model_statistics=MetricsSource(
+#         s3_uri=f"s3://{bucket_processed}/titanic/metrics/metrics.json",
+#         content_type="application/json"
+#     )
+# )
+
+# Define the model using pipeline runtime output
+model = SKLearnModel(
+    model_data=training_step.properties.ModelArtifacts.S3ModelArtifacts,
+    image_uri=sklearn_estimator.training_image_uri(),
+    role=role_arn,
+    sagemaker_session=pipeline_session
+)
+
+# Register the model (deferred via step_args)
+register_model_step = ModelStep(
+    name="RegisterTitanicModel",
+    step_args=model.register(
+        content_types=["text/csv"],
+        response_types=["text/csv"],
+        inference_instances=["ml.t2.medium"],
+        transform_instances=["ml.m5.large"],
+        model_package_group_name="TitanicModelPackageGroup",
+        approval_status="Approved",
+        description="Titanic survival prediction model"
+    )
+)
+
 pipeline = Pipeline(
     name="TitanicPipeline",
-    steps=[processing_step, training_step],
-    sagemaker_session=sagemaker_session
+    steps=[
+        processing_step,
+        training_step,
+        register_model_step
+    ],
+    sagemaker_session=pipeline_session
 )
 
 pipeline.upsert(role_arn=role_arn)
